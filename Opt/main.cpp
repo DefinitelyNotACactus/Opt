e929cd8a203392e6261d6bf2f3f8bbcb63687ce0
DefinitelyNotACactus <galvao2010@icloud.com>
2022-08-27 14:16:21 -0300

    Initial Commit

---
+++ Opt/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Opt
+//
+//  Created by David Galvao on 27/08/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
